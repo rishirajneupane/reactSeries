8ed17ec6c4349d8c1b9be3f1b125abcc2be27c87
rishi <rishirajneupane0125@gmail.com>
2024-03-16 18:14:45 +0545

    React Started

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h2> React Project Started  wow</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chia  from "./Chai"
+
+function App() {
+  
+
+  return (<>
+    <h1>Chai </h1>
+    <h2> Vite bata React Project</h2>
+    </>
+  )
+}
+
+export default App
